e4688f67dc36f4af6139e42d68356ff621a068ec
Marcin Trybała <63187182+MTR94@users.noreply.github.com>
2024-01-30 21:26:42 +0100

    Delete PC22-BazyDanychMaven directory

--- PC22-BazyDanychMaven/sql/hr-postgresql/0-admin.sql
+++
@@ -1,8 +0,0 @@
--- Wykonac jako user postgres (administrator)
--- Pozostale pliki juz jako user hr
-
-DROP DATABASE IF EXISTS hr;
-DROP ROLE IF EXISTS kurs;
-
-CREATE USER kurs PASSWORD 'abc123';
-CREATE DATABASE hr ENCODING 'utf-8' OWNER kurs;
--- PC22-BazyDanychMaven/sql/hr-postgresql/wgraj_hr.sql
+++
@@ -1,5 +0,0 @@
-SET client_encoding = 'UTF8';
-\i 1-hr-create-tabele.sql
-\i 2-hr-create-dodatki.sql
-\i 3-hr-insert.sql
-\i 4-hr-moje-dodatki.sql
